e418b0ae5d13df17eec9768a256e90c1d49617d1
Ashwink1 <mailashwink@gmail.com>
2023-07-29 15:27:49 +0530

    Added tailwind configuration

--- src/components/atoms/Copyright.tsx
+++ src/components/atoms/Copyright.tsx
@@ -5,9 +5,12 @@ import Link from '@mui/material/Link';
 export default function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
+
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Engineerify.io
+      <Link color="inherit" href="https://engineerify.io/">
+        <h1 className="text-3xl font-bold underline">
+          Engineerify.io
+        </h1>
       </Link>{' '}
       {new Date().getFullYear()}.
     </Typography>
