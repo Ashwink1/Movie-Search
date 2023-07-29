8dfcb9001289c7612f0f7a81faa259c853a8b39e
Ashwink1 <mailashwink@gmail.com>
2023-07-29 15:21:08 +0530

    Added material UI configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,44 @@
+
+// Create a movie app similar to the figma design provided.
+// ● Things to be done:
+// ○ The first screen shows you a Discover tab with the movies list. In the prototype
+// link you have all the animation related to cards and search icon.
+// ○ In the sidebar only the Discover tab remains active and the rest remain inactive.
+// ○ The search functionality needs to be implemented when a user clicks on the
+// search icon.
+// ● In the prototype link -
+// ○ By clicking on the first card, you get the details for that movie. You need to
+// implement the same animation for all the cards.
+// ○ When you click on the 6th card, you will be able to know how the cards which
+// are in between open up.
+// ○ Please check the search box animation as well, by clicking on the search icon.
+// ● Create reusable react components (divide the page into components as per your
+// understanding)
+// ● For responsiveness, you can convert the desktop design to mobile according to your
+// own creativity.
+// ● Please use the movies JSON Data provided from the next page to create the exercise.
+// ● Page should be responsive so should look good on desktop and Mobile (no need to
+// think for Tablet)
+// ● We recommend to use MUI for styling.
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Copyright from './components/atoms/Copyright'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Material UI Vite.js example
+        </Typography>
+        <Copyright />
+      </Box>
+    </Container>
   )
 }
 
